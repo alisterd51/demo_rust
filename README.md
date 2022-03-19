3fa083dc35e6c9602395a8d6aabd64e473d355db
anclarma <anclarma@student.42.fr>
2022-03-19 15:58:50 +0100

    ajout demo hello-world

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # demo_rust
+
+rustc file.rs
+rustfmt file.rs
+
+cargo new project_name
+cargo build
+cargo run
+cargo clean
--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,11 +1,11 @@
-use	ferris_says::say;
-use	std::io::{stdout, BufWriter};
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
 
-fn	main() {
-	let stdout = stdout();
-	let message = String::from("Hello fellow Rustaceans!");
-	let width = message.chars().count();
+fn main() {
+    let stdout = stdout();
+    let message = String::from("Hello fellow Rustaceans!");
+    let width = message.chars().count();
+    let mut writer = BufWriter::new(stdout.lock());
 
-	let mut writer = BufWriter::new(stdout.lock());
-	say(message.as_bytes(), width, &mut writer).unwrap();
+    say(message.as_bytes(), width, &mut writer).unwrap();
 }
